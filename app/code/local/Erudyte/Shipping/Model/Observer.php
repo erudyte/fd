2705fe9af09dff76213817817b53a33f0df841cc
Graham Crocker <graham.paul.crocker@gmail.com>
2017-05-17 00:38:12 +0100

    Add Free Shipping Notice to checkout cart

---
+++ app/code/local/Erudyte/Shipping/Model/Observer.php
@@ -0,0 +1,64 @@
+<?php
+
+class Erudyte_Shipping_Model_Observer
+{
+    /**
+     *
+
+     * @param $observer
+     * @return $this
+     * @author Graham Crocker <graham.paul.crocker@gmail.com>
+     */
+    public function displayFreeDeliveryNotice(Varien_Event_Observer $observer)
+    {
+        $shippingConfig = $this->getShippingConfig();
+        if (!$shippingConfig['enabled']) {
+            return;
+        }
+        /** @var Mage_Sales_Model_Quote $quote */
+        $quote = $this->getCheckoutSession()->getQuote();
+        $freeDeliveryMinimum = $shippingConfig['free_delivery_minimum'];
+        //get the quote subtotal
+        $subtotal = $quote->getSubtotal();
+        if ($message = $this->getFreeDeliveryMessage($freeDeliveryMinimum, $subtotal)) {
+            $this->getCheckoutSession()->addNotice($message);
+        }
+    }
+
+
+    /**
+     * @param float $freeDeliveryMinimum
+     * @param float $basketTotal
+     * @return string
+     * @author Graham Crocker <graham.paul.crocker@gmail.com>
+     */
+    protected function getFreeDeliveryMessage($freeDeliveryMinimum, $basketTotal)
+    {
+        $difference = $freeDeliveryMinimum - $basketTotal;
+        if (!((int) $difference > 0)) {
+            $display = "Spend another £$difference to qualify for Free Delivery";
+        } else {
+            $display = '';
+        }
+        return $display;
+    }
+
+    /**
+     * Potential Mocking area
+     *  @author Graham Crocker <graham.paul.crocker@gmail.com>
+     */
+    protected function getShippingConfig()
+    {
+        return Mage::getStoreConfig('shipping_rules/free_delivery');
+    }
+
+    /**
+     * Potential Mocking area
+     * @return Mage_Checkout_Model_Session
+     * @author Graham Crocker <graham.paul.crocker@gmail.com>
+     */
+    protected function getCheckoutSession()
+    {
+        return Mage::getSingleton('checkout/session');
+    }
+}
